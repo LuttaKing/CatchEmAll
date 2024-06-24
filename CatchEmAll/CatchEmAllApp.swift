61f7d00795689d69761839f6eda5c03d59f21c69
LuttaKing <denilsonwashuma@gmail.com>
2024-06-24 20:39:42 +0300

    Initial Commit

---
+++ CatchEmAll/CatchEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllApp.swift
+//  CatchEmAll
+//
+//  Created by Denilson Washuma on 24/06/2024.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
